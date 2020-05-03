a35e93fbfc1df1e7fa77319b3d83eedd0acbf504
piotrprusik <61974584+piotrprusik@users.noreply.github.com>
2020-05-03 19:04:27 +0200

    Update Triangle.java

--- src/main/java/pl/waw/sgh/shapes/Triangle.java
+++ src/main/java/pl/waw/sgh/shapes/Triangle.java
@@ -3,10 +3,11 @@ package pl.waw.sgh.shapes;
 public class Triangle extends Shape implements PerimeterCalculation {
 
     public Triangle (double a, double b, double c) {
-        super(a, b, c);
+        super(a, b, c);    
     }
 
     public double calcSurface(); {
+        // calculating surface using side lenghts only
         double HP = (this.parA + this.parB + this.parC) / 2;
         return Math.sqrt((HP)*(HP-this.parA)*(HP-this.parB)*(HP-this.parC));
     }
